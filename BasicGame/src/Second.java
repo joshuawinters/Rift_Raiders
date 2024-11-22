695f51fa8eea994c867b2a75647e0d210c019353
Domnom22 <553732@student.saxion.nl>
2024-11-22 12:27:10 +0100

    Sprites relatief gemaakt

--- BasicGame/src/Second.java
+++ BasicGame/src/Second.java
@@ -6,20 +6,20 @@ public class Second {
     //sprites Shafir
 
     //shafir stappen naar onder
-    static String imageShafirVoorkant1 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirVoorkant1.png";
-    static String imageShafirVoorkant2 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirVoorkant2.png";
+    static String imageShafirVoorkant1 = "Sprites\\Shafir\\ShafirVoorkant1.png";
+    static String imageShafirVoorkant2 = "Sprites\\Shafir\\ShafirVoorkant2.png";
 
     //shafir stappen naar links
-    static String imageShafirLinks1 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirLinks1.png";
-    static String imageShafirLinks2 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirLinks2.png";
+    static String imageShafirLinks1 = "Sprites\\Shafir\\ShafirLinks1.png";
+    static String imageShafirLinks2 = "Sprites\\Shafir\\ShafirLinks2.png";
 
     //shafir stappen naar boven
-    static String imageShafirAchterkant1 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirAchterkant1.png";
-    static String imageShafirAchterkant2 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirAchterkant2.png";
+    static String imageShafirAchterkant1 = "Sprites\\Shafir\\ShafirAchterkant1.png";
+    static String imageShafirAchterkant2 = "Sprites\\Shafir\\ShafirAchterkant2.png";
 
     //shafir stappen naar rechts
-    static String imageShafirRechts1 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirRechts1.png";
-    static String imageShafirRechts2 = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirRechts2.png";
+    static String imageShafirRechts1 = "Sprites\\Shafir\\ShafirRechts1.png";
+    static String imageShafirRechts2 = "Sprites\\Shafir\\ShafirRechts2.png";
 
     //Shafir idle sprite
     static String imageShafirIdle = "C:\\Users\\juana\\OneDrive\\Bureaublad\\IntelliJ opdrachten\\Sandbox\\BasicGame\\src\\Sprites\\Shafir\\ShafirIdle.png";
