961bfbb77e7ca37b4cd632b9a67d223180cccc5d
joshua <jswinters2003@gmail.com>
2024-12-04 13:02:14 +0100

    Merge branch 'master' of https://github.com/joshuawinters/Rift_Raiders

--- BasicGame/src/tiles/Tiles.java
+++ BasicGame/src/tiles/Tiles.java
@@ -5,7 +5,5 @@ import java.awt.image.BufferedImage;
 public class Tiles {
     public String image;
     public boolean collision = false;
-    public Tiles() {
-        // You can add any initialization logic if needed
-    }
+    public Tiles() {}
 }
