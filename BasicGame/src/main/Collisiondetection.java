6282cf399b88871cd47d49a7caffa978863512d2
shafir <570105@student.saxion.nl>
2024-12-06 13:33:20 +0100

    k

---
+++ BasicGame/src/main/Collisiondetection.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Collisiondetection {
+
+    RiftRaiders gp;
+
+    public Collisiondetection(RiftRaiders gp) {
+        this.gp = gp;
+    }
+
+    public void checkTile(entity.Entity entity) {
+        class Entity {
+            public int worldX;
+            public int worldY;
+
+            public Entity(final Collisiondetection this$0, int x, int y) {
+                this.worldX = x;
+                this.worldY = y;
+            }
+        }
+    }
+}
