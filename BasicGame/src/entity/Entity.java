4c38eb3e86edeab58e3da7d5e59e5bfefd403eeb
shafir <570105@student.saxion.nl>
2024-12-06 13:42:55 +0100

    k

--- BasicGame/src/entity/Entity.java
+++ BasicGame/src/entity/Entity.java
@@ -5,9 +5,11 @@ public class Entity {
 
     public int x; // Position X
     public int y; // Position Y
-    public int speed=8; // Movement speed
+    public int speed = 8; // Movement speed
     public String direction; // Current direction (Up, Down, Left, Right)
     public int stapCounter; // Counter for animation steps
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
 
     protected long animationTime; // Last animation update time
     protected long animationDelay; // Delay between animation frames
@@ -15,6 +17,7 @@ public class Entity {
     //hitbox
     public int width; // Width of the entity
     public int height; // Height of the entity
+    Rectangle staticHitbox;
 
     public Entity(int x, int y, int speed, long animationDelay, int width, int height) {
         this.x = x;
@@ -31,41 +34,55 @@ public class Entity {
 
 
     }
+
     // hit box
     public Rectangle getHitbox() {
-        //putting hitbox in the middle of character
-        // dit werkt kut!!
-        int hitboxX = x + (width*4/5); // Offset to center the hitbox horizontally
-        int hitboxY = y + height; // Offset to center the hitbox vertically
-        int hitboxWidth = width / 2;   // Hitbox is half the sprite's width
-        int hitboxHeight = height / 2; // Hitbox is half the sprite's height
+        int hitboxX = this.x + this.width * 4 / 5;
+        int hitboxY = this.y + this.height;
+        int hitboxWidth = this.width / 2;
+        int hitboxHeight = this.height / 2;
         return new Rectangle(hitboxX, hitboxY, hitboxWidth, hitboxHeight);
     }
 
     public void move(String direction) {
         this.direction = direction;
+        int xRock = this.staticHitbox.x;
+        int yRock = this.staticHitbox.y;
+        int widthRock = this.staticHitbox.width;
+        int heightRock = this.staticHitbox.height;
+        int margin = 10;
         switch (direction) {
             case "Up":
-                y -= speed;
+                if (this.y - this.speed >= yRock + heightRock / 2 + margin || this.y <= yRock || this.x + widthRock / 2 <= xRock - margin || this.x >= xRock + widthRock / 2 + margin) {
+                    this.y -= this.speed;
+                }
                 break;
             case "Down":
-                y += speed;
+                if (this.y + this.speed <= yRock - heightRock / 2 - margin || this.y + this.speed >= yRock + heightRock / 2 + margin || this.x <= xRock - widthRock / 2 - margin || this.x >= xRock + widthRock / 2 + margin) {
+                    this.y += this.speed;
+                }
                 break;
             case "Left":
-                x -= speed;
+                if (this.x - this.speed >= xRock + widthRock / 2 + margin || this.x <= xRock || this.y + heightRock / 2 <= yRock - margin || this.y >= yRock + heightRock / 2 + margin) {
+                    this.x -= this.speed;
+                }
                 break;
             case "Right":
-                x += speed;
-                break;
+                if (this.x + this.speed <= xRock - widthRock / 2 - margin || this.x + this.speed >= xRock + widthRock / 2 + margin || this.y <= yRock - heightRock / 2 - margin || this.y >= yRock + heightRock / 2 + margin) {
+                    this.x += this.speed;
+                }
         }
-    }
 
-    public boolean shouldUpdateAnimation() {
-        long currentTime = System.currentTimeMillis();
-        if (currentTime - animationTime >= animationDelay) {
-            animationTime = currentTime;
-            return true;
+        public boolean shouldUpdateAnimation() {
+            long currentTime = System.currentTimeMillis();
+            if (currentTime - this.animationTime >= this.animationDelay) {
+                this.animationTime = currentTime;
+                return true;
+            } else {
+                return false;
+            }
         }
-        return false;
+
+        public void setStaticHitBox(Rectangle staticHitbox) { this.staticHitbox = staticHitbox; }
     }
 }
