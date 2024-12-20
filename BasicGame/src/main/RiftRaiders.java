77f1c1580bb260c25d3c24972a4e42dec636b342
Domnom22 <553732@student.saxion.nl>
2024-12-20 11:04:31 +0100

    Sprites relatief gemaakt

--- BasicGame/src/main/RiftRaiders.java
+++ BasicGame/src/main/RiftRaiders.java
@@ -87,10 +87,10 @@ public class RiftRaiders implements GameLoop {
         // TODO: make function to load level
         // TODO: make file to create level
         // TODO: each level has a starting posistion, and end position/condition
-        draw_level();
+       // draw_level();
 
         //Stage sprite tekenen
-        //SaxionApp.drawImage(Second.imageStage, 0, 0, 1000, 600);
+        SaxionApp.drawImage(Second.imageStage, 0, 0, 1000, 600);
         //Health boarder tekenen
         SaxionApp.drawImage(Second.imageHealthBoarder, 0, 0, 160, 200);
 
@@ -102,8 +102,8 @@ public class RiftRaiders implements GameLoop {
             SaxionApp.drawImage(Second.imageHartVol3, 105, 55, 30, 30);
         } else if (hartVol == false) {
             SaxionApp.drawImage(Second.imageHartLeeg1, 25, 55, 30, 30);
-            SaxionApp.drawImage(Second.imageHartLeeg2, 25, 55, 30, 30);
-            SaxionApp.drawImage(Second.imageHartLeeg3, 25, 55, 30, 30);
+            SaxionApp.drawImage(Second.imageHartLeeg2, 65, 55, 30, 30);
+            SaxionApp.drawImage(Second.imageHartLeeg3, 105, 55, 30, 30);
         }
 
         //sprite inspawnen voor knuppel en boolean koppelen
@@ -192,6 +192,7 @@ public class RiftRaiders implements GameLoop {
                         SaxionApp.drawImage(Second.imageCavemanSlagVoor, caveman.x, caveman.y, 100, 100);
                     }
                     charHIt = true;
+                    hartVol = false;
                 } else {
                     if (caveman.direction.equals("Left")) {
                         SaxionApp.drawImage(Second.imageCavemanLinks1, caveman.x, caveman.y, 100, 100);
@@ -232,7 +233,7 @@ public class RiftRaiders implements GameLoop {
             }
         }
 
-        //damage system aanmaken
+        //hit indicator aanmaken
         if (knuppelOpgepakt) {
             if (charHIt && damageRefreshChar > 0) {
                 if (shafir.direction.equals("Left")) {
@@ -245,14 +246,12 @@ public class RiftRaiders implements GameLoop {
                     SaxionApp.drawImage(Second.imageShafirDamageVoor, shafir.x, shafir.y, 100, 100);
                 }
 
-                if (damageRefreshChar <= 0) {
+
+                if (charHIt) {
+                    charHIt = false;
                     damageRefreshChar = 5;
-                    if (charHIt) {
-                        charHIt = false;
-                    } else {
-                        charHIt = true;
-                    }
                 }
+
                 damageRefreshChar--;
             } else if (damageRefreshChar < 0) {
                 String sprite2 = switch (shafir.direction) {
@@ -269,7 +268,6 @@ public class RiftRaiders implements GameLoop {
                 SaxionApp.drawImage(sprite2, shafir.x, shafir.y, 100, 100);
             }
         }
-
 
 
         // Detect collision
